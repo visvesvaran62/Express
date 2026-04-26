81f90de9753109733ad25ebd41d8a28db8769dcb
visvesvaran62 <visvesvaran62@gmail.com>
2026-04-26 16:06:51 +0530

    updated

--- day-44/backend/controller/authControlle.js
+++ day-44/backend/controller/authControlle.js
@@ -1,7 +1,14 @@
  import bcrypt from "bcrypt"
+ import userModel from "../models/userModel.js"
 export const createUser= async(req,res)=>{
     const {userName,email,password}=req.body
+
+   const emailcheck= await userModel.findOne({email})
+        if(emailcheck){
+            return res.status(400).json({msg: "already register"})
+        }
     
+  
 
  const hashingpass= await bcrypt.hash(password,10)
     try {
